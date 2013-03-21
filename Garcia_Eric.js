2dbd1546dd3a7dac8664565908ae7d7cee85bda0
eng618 <eng618@fullsail.edu>
2013-03-20 23:47:50 -0400

    Initial stroy set up

--- Garcia_Eric.js
+++ Garcia_Eric.js
@@ -3,3 +3,47 @@
 //Eric Garcia
 //SDI 1303
 //Project 3
+//Story - Wedding
+
+
+var occation = "wedding";
+
+//json Data
+var guestList = {
+	"guests":[
+		{	guestFirstName: "John",
+			guestLastName: 	"Anderson", 
+		},
+		{	guestFirstName: "Jane",
+			guestLastName: 	"Anderson" 	
+		},
+		{	guestFirstName: "Mike",
+			guestLastName: 	"Smith" 	
+		},
+		{	guestFirstName: "Ashley",
+			guestLastName: 	"Smith" 	
+		},
+		{	guestFirstName: "Jacob",
+			guestLastName: 	"Lopez" 	
+		},
+		{	guestFirstName: "Lesly",
+			guestLastName: 	"Lopez" 	
+		},
+		{	guestFirstName: "Kane",
+			guestLastName: 	"Sims" 	
+		},
+		{	guestFirstName: "Janet",
+			guestLastName: 	"Sims" 	
+		},
+		{	guestFirstName: "Alberto",
+			guestLastName: 	"Hernandez" 	
+		},
+		{	guestFirstName: "Alicia",
+			guestLastName: 	"Hernandez" 	
+		}
+	]
+};
+
+//test data
+console.log ( "My future bride and I are planning out our " + occation + ".");
+console.log ( guestList );
