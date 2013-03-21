6e197bd8ff19d3da63249eb1ddb1dc3189ac9f8f
eng618 <eng618@fullsail.edu>
2013-03-21 02:49:25 -0400

    Added Object with Method Accessor & Method Mutator
    Also added additional json data and organized existing json data.

--- Garcia_Eric.js
+++ Garcia_Eric.js
@@ -5,14 +5,15 @@
 //Project 3
 //Story - Wedding
 
-
+//Variables
 var occation = "wedding";
+var music	 = [];
 
 //json Data
 var guestList = {
 	"guests":[
 		{	guestFirstName: "John",
-			guestLastName: 	"Anderson", 
+			guestLastName: 	"Anderson" 
 		},
 		{	guestFirstName: "Jane",
 			guestLastName: 	"Anderson" 	
@@ -41,9 +42,83 @@ var guestList = {
 		{	guestFirstName: "Alicia",
 			guestLastName: 	"Hernandez" 	
 		}
-	]
-};
+	]//end guests
+};//end guestList
+var bridalParty = {
+	"groomsMen":[
+		{	firstName: 		"John",
+			LastName: 		"Doe",
+			isBestMan: 		true
+		},
+		{	firstName: 		"John",
+			LastName: 		"Doe",
+			isBestMan: 		false
+		},
+		{	firstName: 		"John",
+			LastName: 		"Doe",
+			isBestMan: 		false
+		},
+		{	firstName: 		"John",
+			LastName: 		"Doe",
+			isBestMan: 		false
+		}
+	],//ends groomsMen
+	"bridesMades":[
+		{	firstName: 		"Jane",
+			lastName: 		"Doe",
+			isMadeOfHoner: 	true
+		},
+		{	firstName: 		"Jane",
+			lastName: 		"Doe",
+			isMadeOfHoner: 	false
+		},
+		{	firstName: 		"Jane",
+			lastName: 		"Doe",
+			isMadeOfHoner: 	false
+		},
+		{	firstName: 		"Jane",
+			lastName: 		"Doe",
+			isMadeOfHoner: 	false
+		}
+	]//end bridesMades
+};//end bridalParty
+
+//object
+var food = {
+
+	"horderves"	: 0.50,
+	"entree" 	: 2.75,
+	"kidsMeal"	: 1.25,
+	"cake"		: 1.00,
+	"totalCost"	: function(){ //Method: Accessor (Getter)
+		var costOfFood = this.horderves + this.entree + this.kidsMeal + this.cake
+		return costOfFood;
+	},//end totalCost method
+	"otherEntree" : function (alternateEntree){ //Method: Mutator (Setter)
+		this.entree = alternateEntree; 
+	}
+
+};//end food object
+
+//Main code
+console.log ( "My future bride and I are planning out our " + occation + ".");
+console.log ( "The estimate cost for food per person is $" + food.totalCost() );
+
+food.otherEntree(2.40);
+
+console.log ( "With the other entree we liked, it would be $" + food.totalCost());
+
+
+
+
+
+
+
+
+
+
+
 
 //test data
-console.log ( "My future bride and I are planning out our " + occation + ".");
 console.log ( guestList );
+console.log ( bridalParty );
