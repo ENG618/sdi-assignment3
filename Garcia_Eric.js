b089ec0b61dc2d439ce8892e3205ab9204b7c3cb
eng618 <eng618@fullsail.edu>
2013-03-21 23:18:08 -0400

    Coded bestMan/maidOfHonor
    Further organized, and personalized the json date, updated food cost to
    be more realistic, removed test data from bottom of code.
    
    Updated html to reflect actual story

--- Garcia_Eric.js
+++ Garcia_Eric.js
@@ -8,10 +8,48 @@
 //Variables
 var bride = "Lindsay"
 var groom = "Eric"
-var occation = "wedding";
+var occasion = "wedding";
 var music	 = [];
 
 //json Data
+var bridalParty = {
+	"groomsMen":[
+		{	firstName: 		"Jack",
+			lastName: 		"Hill",
+			isBestMan: 		true
+		},
+		{	firstName: 		"Mark",
+			lastName: 		"Twain",
+			isBestMan: 		false
+		},
+		{	firstName: 		"Hunter",
+			lastName: 		"White",
+			isBestMan: 		false
+		},
+		{	firstName: 		"Brad",
+			lastName: 		"Copper",
+			isBestMan: 		false
+		}
+	], //ends groomsMen
+	"bridesMades":[
+		{	firstName: 		"Jill",
+			lastName: 		"Freeman",
+			isMadeOfHoner: 	true
+		},
+		{	firstName: 		"Kelly",
+			lastName: 		"Long",
+			isMadeOfHoner: 	false
+		},
+		{	firstName: 		"Ann",
+			lastName: 		"Lee",
+			isMadeOfHoner: 	false
+		},
+		{	firstName: 		"Charlotte",
+			lastName: 		"Finn",
+			isMadeOfHoner: 	false
+		}
+	] //end bridesMades
+}; //end bridalParty
 var guestList = {
 	"guests":[
 		{	firstName: "John",
@@ -46,52 +84,14 @@ var guestList = {
 		}
 	] //end guests
 }; //end guestList
-var bridalParty = {
-	"groomsMen":[
-		{	firstName: 		"John",
-			lastName: 		"Doe",
-			isBestMan: 		true
-		},
-		{	firstName: 		"John",
-			lastName: 		"Doe",
-			isBestMan: 		false
-		},
-		{	firstName: 		"John",
-			lastName: 		"Doe",
-			isBestMan: 		false
-		},
-		{	firstName: 		"John",
-			lastName: 		"Doe",
-			isBestMan: 		false
-		}
-	], //ends groomsMen
-	"bridesMades":[
-		{	firstName: 		"Jane",
-			lastName: 		"Doe",
-			isMadeOfHoner: 	true
-		},
-		{	firstName: 		"Jane",
-			lastName: 		"Doe",
-			isMadeOfHoner: 	false
-		},
-		{	firstName: 		"Jane",
-			lastName: 		"Doe",
-			isMadeOfHoner: 	false
-		},
-		{	firstName: 		"Jane",
-			lastName: 		"Doe",
-			isMadeOfHoner: 	false
-		}
-	] //end bridesMades
-}; //end bridalParty
 
 //object
 var food = {
 
-	"horderves"	: 0.50,
-	"entree" 	: 2.75,
-	"kidsMeal"	: 1.25,
-	"cake"		: 1.00,
+	"horderves"	: 5.30,
+	"entree" 	: 10.50,
+	"kidsMeal"	: 5.50,
+	"cake"		: 3.18,
 	"totalCost"	: function(){ //Method: Accessor (Getter)
 		var costOfFood = this.horderves + this.entree + this.kidsMeal + this.cake
 		return costOfFood;
@@ -102,34 +102,46 @@ var food = {
 
 }; //end food object
 
+
+//Main code
+console.log ( "My future bride (" + bride + ") and I are planning our " + occasion + ".");
+
+//Bridal party
+var bestMan = bridalParty
+for (var key in bestMan
+.groomsMen){
+	if (bestMan
+	.groomsMen[key].isBestMan == true){
+		console.log ( bestMan.groomsMen[key].firstName + " is the best man." )
+	}else{
+		console.log ( bestMan.groomsMen[key].firstName + " isn't the best man, but is one of the groomsman.")
+	}
+}; // end bestMan
+var maidOfHonr = bridalParty
+for (var key in maidOfHonr.bridesMades){
+	if (maidOfHonr.bridesMades[key].isMadeOfHoner == true){
+		console.log ( maidOfHonr.bridesMades[key].firstName + " is the Made of Honor." )
+	}else{
+		console.log ( maidOfHonr.bridesMades[key].firstName + " isn't the Maid of Honor, but is one of the Brides Maids.")
+	}
+}; // end maidOfHonr
+
 var totalAttendees = function (){
 
 };
-//Main code
-console.log ( "My future bride (" + bride + ") and I are planning our " + occation + ".");
 
+// Food cost
 console.log ( "The estimated cost of food per person is $" + food.totalCost() );
-food.otherEntree(2.40);
+food.otherEntree(15.35);
 console.log ( "With the other entree we liked, it would be $" + food.totalCost());
 
 console.log ( groom + " said, who all are we inviting to the wedding?" )
+
 var newObj = guestList
 for (var key in newObj.guests){
 	if (newObj.guests[key].firstName == newObj.guests[0].firstName){
 		console.log ( bride + " replied, " + newObj.guests[key].firstName )
 	}else{
-		console.log ( newObj.guests[key].firstName)
+		console.log ( newObj.guests[key].firstName )
 	}
-};
-
-
-
-
-
-
-
-
-//test data
-console.log ( "Test logs" );
-console.log ( guestList );
-console.log ( bridalParty );
+};// end newObj
